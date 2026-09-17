16623b5fb48a038905dd853420587bdc9369b14e
Klaus <ndjunce@hotmail.com>
2026-09-16 20:05:45 -0500

    Archive Week 1 to season.js so its tab stays viewable after advancing to Week 2 (fixes greyed-out Week 1)

--- season.js
+++ season.js
@@ -24,11 +24,27 @@ window.SEASON = {
 
   // One entry per COMPLETED week.
   weeks: [
-    // Example (delete once you add real weeks):
-    // { week: 1,
-    //   results: { Nick:{w:11,l:5}, Clyde:{w:9,l:7}, Chet:{w:10,l:6},
-    //              Henry:{w:8,l:8}, Riley:{w:12,l:4}, Bobby:{w:7,l:9} },
-    //   tiebreaker: { Nick:44, Clyde:51, Chet:47, Henry:40, Riley:45, Bobby:52 },
-    //   tbActual: 48 },
+    { week: 1,
+      results: { Nick:{w:11,l:5}, Clyde:{w:9,l:7}, Chet:{w:11,l:5}, Henry:{w:9,l:7}, Riley:{w:9,l:7}, Bobby:{w:12,l:4} },
+      tiebreaker: { Nick:43, Clyde:47, Chet:50, Henry:42, Riley:49, Bobby:40 },
+      tbActual: 41,
+      games: [
+      { team1:"NE", team2:"SEA", winner:"SEA", picks:{ Nick:"NE", Clyde:"SEA", Chet:"SEA", Henry:"NE", Riley:"SEA", Bobby:"SEA" } },
+      { team1:"SF", team2:"LAR", winner:"SF", picks:{ Nick:"SF", Clyde:"LAR", Chet:"LAR", Henry:"LAR", Riley:"LAR", Bobby:"LAR" } },
+      { team1:"TB", team2:"CIN", winner:"CIN", picks:{ Nick:"CIN", Clyde:"CIN", Chet:"CIN", Henry:"CIN", Riley:"TB", Bobby:"CIN" } },
+      { team1:"NO", team2:"DET", winner:"DET", picks:{ Nick:"DET", Clyde:"NO", Chet:"DET", Henry:"DET", Riley:"DET", Bobby:"DET" } },
+      { team1:"NYJ", team2:"TEN", winner:"NYJ", picks:{ Nick:"NYJ", Clyde:"NYJ", Chet:"TEN", Henry:"TEN", Riley:"TEN", Bobby:"NYJ" } },
+      { team1:"BAL", team2:"IND", winner:"BAL", picks:{ Nick:"BAL", Clyde:"BAL", Chet:"BAL", Henry:"IND", Riley:"BAL", Bobby:"BAL" } },
+      { team1:"ATL", team2:"PIT", winner:"PIT", picks:{ Nick:"PIT", Clyde:"ATL", Chet:"PIT", Henry:"PIT", Riley:"PIT", Bobby:"PIT" } },
+      { team1:"CHI", team2:"CAR", winner:"CHI", picks:{ Nick:"CHI", Clyde:"CHI", Chet:"CHI", Henry:"CHI", Riley:"CHI", Bobby:"CHI" } },
+      { team1:"CLE", team2:"JAX", winner:"JAX", picks:{ Nick:"JAX", Clyde:"JAX", Chet:"JAX", Henry:"JAX", Riley:"JAX", Bobby:"JAX" } },
+      { team1:"BUF", team2:"HOU", winner:"BUF", picks:{ Nick:"HOU", Clyde:"BUF", Chet:"BUF", Henry:"BUF", Riley:"HOU", Bobby:"HOU" } },
+      { team1:"MIA", team2:"LV", winner:"LV", picks:{ Nick:"LV", Clyde:"MIA", Chet:"MIA", Henry:"LV", Riley:"LV", Bobby:"LV" } },
+      { team1:"GB", team2:"MIN", winner:"MIN", picks:{ Nick:"MIN", Clyde:"MIN", Chet:"MIN", Henry:"MIN", Riley:"MIN", Bobby:"MIN" } },
+      { team1:"WSH", team2:"PHI", winner:"PHI", picks:{ Nick:"PHI", Clyde:"PHI", Chet:"PHI", Henry:"PHI", Riley:"PHI", Bobby:"PHI" } },
+      { team1:"ARI", team2:"LAC", winner:"ARI", picks:{ Nick:"LAC", Clyde:"LAC", Chet:"LAC", Henry:"LAC", Riley:"LAC", Bobby:"LAC" } },
+      { team1:"DAL", team2:"NYG", winner:"NYG", picks:{ Nick:"DAL", Clyde:"DAL", Chet:"NYG", Henry:"DAL", Riley:"DAL", Bobby:"DAL" } },
+      { team1:"DEN", team2:"KC", winner:"KC", picks:{ Nick:"DEN", Clyde:"DEN", Chet:"DEN", Henry:"DEN", Riley:"DEN", Bobby:"KC" } }
+      ] },
   ],
 };
